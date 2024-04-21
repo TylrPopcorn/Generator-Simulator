757fc2209ba92d4cea78f3c0a4046ff7efd2ecaa
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-04-21 09:39:49 -0400

    Begin development

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+/*
+ Dependencies:
+ -------------
+ npm install react
+ npm install parcel-bundler
+
+*/
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
