b2e12b4a153977b934bda8046933890ff4524d34
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-04-23 19:22:29 -0400

    working on

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+/*
+ Dependencies:
+ -------------
+ npm install react
+ npm install parcel-bundler
+ npm install react-router-dom
+*/
+import HOME from "./src/HOME";
+//
+//vars:
+const TITLE = "GENERATOR SIMULATOR"; /* Change the title to whatever */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <HOME />
+  </BrowserRouter>
+);
