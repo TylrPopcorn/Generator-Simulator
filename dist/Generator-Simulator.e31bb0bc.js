eb814fbe2124f6ad7d5ba88df830a763685da30a
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-05-21 20:41:34 -0400

    basic functionality

--- dist/Generator-Simulator.e31bb0bc.js
+++ dist/Generator-Simulator.e31bb0bc.js
@@ -37033,7 +37033,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51648" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61216" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
