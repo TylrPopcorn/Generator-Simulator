87f87c4c0a5dbcb189e4539a48d163786a3d72b2
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-06-23 16:26:33 -0400

    finished number gen

--- simulators/Number/scripts/Functions.ts
+++ simulators/Number/scripts/Functions.ts
@@ -1,21 +1,35 @@
 //All background functions that support the main numberGenerator file.
-//--- imports:]
+//--[ IMPORTS ]:--
 import wait from "./wait";
-//
-const Functions = {
-  //All exported functions within one place.
-};
 
+//--[ TYPES ]:--
+interface FunctionsType {
+  [key: string]: Function;
+}
+
+//-----[ VARIABLES ]:-----
+const Functions: FunctionsType = {
+  //All returning functions in one place.
+  //-----------------------------------
+  // func Clicked()
+};
 //----------------                  ------------------------------                      ----------------------------
 //----------------                  ---------[ FUNCTIONS ]--------                      ----------------------------
 //
+
 Functions["Clicked"] = function () {
   //Each time the button gets clicked on.
   //----------------
-  const NumberHolder = document.querySelector(".NumberHolder");
-  const Numb = document.querySelector(".randomNumber"); //grab number area.
 
-  //Fade/Shadow effects (ADD):
+  const NumberHolder = document.querySelector(".NumberHolder") as HTMLElement;
+  const Numb = document.querySelector(".randomNumber") as HTMLElement;
+
+  // Ensure elements are found
+  if (!NumberHolder || !Numb) {
+    throw new Error("Cannot find corresponding HTML Elements");
+  }
+
+  //Fade | Shadow effects:
   Numb.classList.add("fade");
   NumberHolder.classList.add("shadowed");
 
@@ -30,6 +44,8 @@ Functions["Clicked"] = function () {
   });
 };
 
-//------[ EXPORTS ]:
+//----------------                  ------------------------------                      ----------------------------
+
+//-----[ EXPORTS ]: --]
 export default Functions;
-//-----------------
+//---------------------
